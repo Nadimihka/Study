48aee051b114969d0702c5836d4da6a3dad679a8
Nadezhda Krutova <nadiya-yand@yandex.ru>
2022-08-02 15:55:20 +0300

    Added demonstration of SalesManager

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-
+        // массив прибыли по месяцам
+        int[] profit = {120, 300, 460, 1280, 500, 720,
+                2050, 320, 7000, 600, 840, 350};
+        SalesManager salesManager = new SalesManager(profit);
+        System.out.println("Размер максимальной прибыли " +
+                salesManager.max() + " руб.");
     }
 }
